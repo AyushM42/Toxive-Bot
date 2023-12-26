5d67a2458072af944d5f992bc89cef9534a66bd4
Ayush Mishra <ayushm4@uci.edu>
2023-12-25 19:17:59 -0800

    Added timing to bot, should only send message if its after dusk or before dawn

--- bot.py
+++ bot.py
@@ -1,12 +1,20 @@
 import discord
+import pytz
 import responses
 import datetime
+from astral import Astral
 
 async def send_message(message, user_message):
     try:
-        if responses.handle_response(user_message):
-            await message.channel.send("The night is still young...")
-            await message.channel.send("https://tenor.com/view/batman-gif-4439279616571508647")
+
+        a = Astral()
+        city = a['Irvine']
+        now = datetime.now(pytz.pst)
+        sun = city.sun(date=now, local=True)
+        if now >=sun['dusk'] or now <= sun['dawn']:
+            if responses.handle_response(user_message):
+                await message.channel.send("The night is still young...")
+                await message.channel.send("https://tenor.com/view/batman-gif-4439279616571508647")
     except Exception as e:
         print(e)
 
