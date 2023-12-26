27bbe1e9487e6d348cd96aa7de9e1b8aa5732874
Ayush Mishra <ayushm4@uci.edu>
2023-12-25 20:54:23 -0800

    Modified Responses to include more words

--- responses.py
+++ responses.py
@@ -1,5 +1,5 @@
 def handle_response(message):
-    night_strings = ["tired", "goodnight", "good night", "sleepy", "late", "night"]
+    night_strings = ["tired", "goodnight", "good night", "sleep", "late", "night", "bed", ]
     for ns in night_strings:
         if ns in message.lower():
             return True
