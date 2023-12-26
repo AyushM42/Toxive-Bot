ab95171211c4da78682ec6097c0401218f6e31ad
Ayush Mishra <ayushm4@uci.edu>
2023-12-25 21:30:13 -0800

    Added "Gn"

--- responses.py
+++ responses.py
@@ -1,5 +1,5 @@
 def handle_response(message):
-    night_strings = ["tired", "goodnight", "good night", "sleep", "late", "night", "bed", ]
+    night_strings = ["tired", "goodnight", "good night", "sleep", "late", "night", "bed", "gn"]
     for ns in night_strings:
         if ns in message.lower():
             return True
